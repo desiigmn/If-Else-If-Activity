4e4629b471ff494bc0f2c9f2a6f5df0df4a4ac54
Desiree Ann Guzman <desireeann000@gmail.com>
2024-10-07 21:32:46 +0800

    Commit with gradeAssignment.js

---
+++ gradeAssignment.js
@@ -0,0 +1,13 @@
+function gradeAssignment() {
+    let score = prompt("Enter your score:");
+
+    if (score >= 90) {
+    alert("Excellent");
+    } else if (score >= 80 && score <= 89) {
+    alert("Good");
+    } else if (score >= 70 && score <= 79) {
+    alert("Fair");
+    } else {
+    alert("Needs Improvement");
+    }
+}
